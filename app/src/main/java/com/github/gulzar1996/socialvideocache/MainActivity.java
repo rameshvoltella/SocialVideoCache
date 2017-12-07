7db9a7bd8fb1209e66f04da7a359e72d22defb23
Gulzar Ahmed <gulzarhome54@gmail.com>
2017-12-08 00:33:40 +0530

    Exoplayer call back added

--- app/src/main/java/com/github/gulzar1996/socialvideocache/MainActivity.java
+++ app/src/main/java/com/github/gulzar1996/socialvideocache/MainActivity.java
@@ -1,13 +1,136 @@
 package com.github.gulzar1996.socialvideocache;
 
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    SimpleExoPlayerView simpleExoPlayerView;
+    private SimpleExoPlayer player;
+    VideoDownloadAndPlayService videoService;
+    private DefaultTrackSelector trackSelector;
+    private BandwidthMeter bandwidthMeter;
+    private boolean shouldAutoPlay;
+    private String videoPath="https://socialcops.com/images/old/spec/home/header-img-background_video-1920-480.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bandwidthMeter = new DefaultBandwidthMeter();
+        shouldAutoPlay = true;
+        startProxy();
+    }
+
+    private void startProxy() {
+//
+        videoService = VideoDownloadAndPlayService.startServer(this, videoPath, new VideoDownloadAndPlayService.VideoStreamInterface()
+        {
+            @Override
+            public void onServerStart(String videoStreamUrl)
+            {
+                Toast.makeText(MainActivity.this, videoStreamUrl, Toast.LENGTH_SHORT).show();
+                MediaSource mediaSource = buildMediaSource(Uri.parse(videoStreamUrl));
+                player.prepare(mediaSource);
+
+            }
+        });
+
+
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            shouldAutoPlay = player.getPlayWhenReady();
+            player.release();
+            player = null;
+            trackSelector = null;
+        }
+    }
+
+
+
+    private void initializePlayer() {
+
+        simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.videoView);
+        simpleExoPlayerView.requestFocus();
+
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+
+        trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+        simpleExoPlayerView.setPlayer(player);
+
+        player.setPlayWhenReady(shouldAutoPlay);
+/*        MediaSource mediaSource = new HlsMediaSource(Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"),
+                mediaDataSourceFactory, mainHandler, null);*/
+
+        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+
+
+    }
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        if(videoService != null)
+            videoService.stop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource(uri,
+                new DefaultHttpDataSourceFactory("ua"),
+                new DefaultExtractorsFactory(), null, null);
     }
 }
